27c1eb7e3fc4c0b960d63247ccd2f3ededa8693c
Leif DeJong <leifdejong@gmail.com>
2018-10-18 09:47:11 -0500

    Adding build procedure

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,19 @@
 const path = require('path');
+const pkg = require('./package.json');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
-const pkg = require('./package.json');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 const DEV_MODE = process.env.NODE_ENV === 'development';
 
 module.exports = {
-  devtool: DEV_MODE ? 'eval' : 'source-map',
-  entry: './src/browser',
+  devtool: DEV_MODE ? 'inline-source-map' : 'source-map',
+  entry: {
+    browser: './src/browser',
+  },
   output: {
-    path: path.resolve(__dirname, '/dist'),
-    filename: `${pkg.name}.js`,
+    path: path.resolve(__dirname, 'dist'),
+    filename: `${pkg.name}-[name].js`,
     libraryTarget: 'var',
     library: 'TmntApi',
   },
@@ -23,13 +26,34 @@ module.exports = {
       },
     ],
   },
-  plugins: [
-    new CleanWebpackPlugin(['dist']),
-    new HtmlWebPackPlugin({
-      title: 'TMNT API Client',
-      filename: 'index.html',
-      template: 'tests/broswer.ejs',
-      favicon: false,
-    }),
-  ],
+  optimization: {
+    minimizer: [new UglifyJsPlugin({
+      sourceMap: true,
+      uglifyOptions: {
+        warnings: false,
+        output: {
+          comments: false
+        },
+      }
+    })],
+  },
+  performance: {
+    hints: false
+  },
+  plugins: (function() { 
+    var plugins = [
+      new CleanWebpackPlugin(['dist'])
+    ];
+
+    if (process.env.NODE_ENV === 'development') {
+      plugins.push(new HtmlWebPackPlugin({
+        title: 'TMNT API Client',
+        filename: 'index.html',
+        template: 'tests/broswer.ejs',
+        favicon: false,
+      }));
+    }
+
+    return plugins;
+  })(),
 };
