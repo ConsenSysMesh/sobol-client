8e2dd9b5dff071655b41272e78c81d0f8faec961
Leif DeJong <leifdejong@gmail.com>
2019-04-28 22:50:27 -0500

    Updated build strategy

---
+++ tests/deprecations.js
@@ -0,0 +1,42 @@
+const Client = require('../');
+const Base = require('../base');
+
+const RsaClient = new Base();
+const { log, error } = console;
+const apiKey = '';
+const rsa = {
+  kid: '...',
+  private: '...',
+};
+
+// rsa usage
+RsaClient.connect({
+  key: rsa,
+  protocol: 'http',
+  host: 'localhost',
+})
+  .catch((err) => {
+    error('Client Error', err.message);
+  });
+
+// basic usage
+Client.connect({
+  key: apiKey,
+  protocol: 'http',
+  host: 'localhost',
+})
+  .then(() => {
+    log('Version:', Client.getVersion());
+
+    return Client.setKey(apiKey)
+      .then((updatedClient) => {
+        log('Set New Keys:', updatedClient._key);
+
+        updatedClient.setToken(apiKey);
+        log('Set New Token:', updatedClient.getSession());
+
+        updatedClient.setOrg('NewOrg');
+        log('Set New Org:', updatedClient._orgId);
+      });
+  })
+  .catch(e => error(e));
